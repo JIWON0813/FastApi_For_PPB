256d6f2f8b7dd5a8543b5d47559474874c45a97b
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2022-08-14 19:28:56 +0900

    feat : 리플렉션 구조 구현 완료

---
+++ company/toss/urls.py
@@ -0,0 +1,5 @@
+class Toss():
+    def aa(self):
+        #scraping
+        a = "toss"
+        return a
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import fastapi
+import importlib
+from datetime import date
 
 app = fastapi.FastAPI()
 
 @app.get('/')
 def home():
-	return {"message": "Welcome Home!"}
+	return date.today()
+
+
+@app.get('/datas')
+def datas():   
+    mod = importlib.import_module('company.'+'toss'+ '.urls')
+    dic = {}
+    
+    result = getattr(mod, 'Toss')()
+    dic['Toss'] = result.aa()
+    
+    return dic
