cffb78eea1e361919286f937c5f7dce4f4437c05
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2022-08-14 21:48:52 +0900

    feat : 토스 테크블로그 최신 날짜 파싱

--- company/toss/urls.py
+++ company/toss/urls.py
@@ -1,5 +1,15 @@
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+
 class Toss():
     def getNewDate(self):
         #scraping
-        a = "toss"
-        return a
+        url = "https://toss.tech/"
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        data = soup.find('p', attrs={'class' : 'css-10958ez e1b9mov10'}).text
+        result = datetime.datetime.strptime(data, '%Y. %m. %d')
+
+        return result
